8b28b7ac85067bb7cf18294cda433f0541f55e40
SarahFDAK <47902741+SarahFDAK@users.noreply.github.com>
2019-08-29 13:47:10 -0800

    Add hello.cpp

---
+++ hw0/hello/hello.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Hello
+//
+//  Created by Sarah Carter on 8/29/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "Hello, World!\n";
+    }
+    return 0;
+}
