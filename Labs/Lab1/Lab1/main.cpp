177350384258e6fb8dfbbc9f0f55bd6666320499
Sarah Carter <sarah.c@fdak.net>
2019-08-30 10:09:11 -0800

    Update changes to lab 1

--- Labs/Lab1/Lab1/main.cpp
+++ Labs/Lab1/Lab1/main.cpp
@@ -1,13 +1,20 @@
 //
 //  main.cpp
-//  Lab1
 //  Sarah Carter
+//  8/30/19
 //  Lab 1 CS201
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(int argc, const char * argv[])
+{
     std::cout << "Hello, World!\n";
+    
+    int a = 145;
+    int b = 23;
+    int c = a * b;
+    
+    std::cout << "a * b = " << a << " * " << b << " = " << c << ".\n";
+    
     return 0;
 }
