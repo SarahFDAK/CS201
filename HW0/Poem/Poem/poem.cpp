7561a614bcb65f5a532d80de6b2de7363c635800
Sarah Carter <sarah.c@fdak.net>
2019-08-30 10:15:51 -0800

    Updates to poem.cpp title and description.

--- HW0/Poem/Poem/poem.cpp
+++ HW0/Poem/Poem/poem.cpp
@@ -1,15 +1,13 @@
 //
 //  poem.cpp
-//  Poem
-//
-//  Created by Sarah Carter on 8/29/19.
-//  Copyright © 2019 Sarah Carter. All rights reserved.
-//
+//  Sarah Carter
+//  8/29/19
+//  This program prints a poem that I like.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(int argc, const char * argv[])
+{
     std::cout << "Some primal termite knocked on wood, and tasted it, and found it good. That is why your cousin Mae fell through the parlor floor today.\n";
     return 0;
 }
