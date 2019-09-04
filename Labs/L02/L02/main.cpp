ea501e579604d73cc53ea6b9e9116e1de5312951
Sarah Carter <sarah.c@fdak.net>
2019-09-04 10:01:16 -0800

    Commit part 1 of Lab 2

---
+++ Labs/L02/L02/main.cpp
@@ -0,0 +1,20 @@
+//
+//  SCLab02.cpp
+//  Sarah Carter
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    int a,b;
+    std::cout << "Enter an integer: \n";
+    std::cin >> a;
+    
+    b=a+1;
+    
+    std::cout<< "You entered "<<a<<". The square of the next number is " << b*b <<std::endl;
+    
+    return 0;
+}
+
