e6e7a554036efe7c9a85ba0122c88d304e17f305
Sarah Carter <sarah.c@fdak.net>
2019-09-04 11:34:11 -0800

    Update lab2 part 2 with leading zeroes

--- Labs/L02/L2p2/L2p2/main.cpp
+++ Labs/L02/L2p2/L2p2/main.cpp
@@ -4,17 +4,19 @@
 //
 
 #include <iostream>
+#include <iomanip>
 
 int main(int argc, const char * argv[]) {
     
     int mo, day, year;
+    
     std::cout<< "Enter your birth month as an integer:"<<std::endl;
-    std::cin>> mo;
+    std::cin >> mo;
     std::cout<< "Enter the day of the month of your birthday:\n";
     std::cin >> day;
     std::cout<< "Enter the year you were born:"<<std::endl;
     std::cin>> year;
     std::cout<< "Your birthday is "<< mo << "/"<<day<<"/"<<year<<std::endl;
-    std::cout<< year <<"-"<< mo << "-" << day << std::endl;
+    std::cout<< year <<"-"<< std::setfill('0') << std::setw(2) << mo << "-" << std::setfill('0') << std::setw(2) << day << std::endl;
     return 0;
 }
