3cf498581c97f1d8cd6433c73b9bee869c89ca9f
Sarah Carter <sarah.c@fdak.net>
2019-09-11 10:11:50 -0800

    Commit lab04add

---
+++ Labs/Lab04/lab04add/lab04add/lab04add.cpp
@@ -0,0 +1,19 @@
+//
+//  lab04add.cpp
+//  Sarah Carter
+//  9/11/19
+//  This prints prints the numbers 1 to 100, except it prints "Fizz" if the number is divisible by 3, "Buzz" if the number is divisible by 5, and "Fizz Buzz" if the number is divisible by both 3 and 5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    for(int i = 1; i <= 100; i++)
+    {
+        if()
+        {
+            std::cout << "fizz\n";
+        }
+    }
+    return 0;
+}
