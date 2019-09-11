9424167cf3ed9105a98b3815bcbce89a6858d097
Sarah Carter <sarah.c@fdak.net>
2019-09-11 12:08:57 -0800

    Commit completed lab04 additional

--- Labs/Lab04/lab04add/lab04add/lab04add.cpp
+++ Labs/Lab04/lab04add/lab04add/lab04add.cpp
@@ -10,9 +10,23 @@
 int main(int argc, const char * argv[]) {
     for(int i = 1; i <= 100; i++)
     {
-        if()
+        int f = i%3;
+        int b = i%5;
+        if(b==0 && f==0)
+        {
+            std::cout << "fizz buzz\n";
+        }
+        else if (f==0)
         {
             std::cout << "fizz\n";
+        }
+        else if (b==0)
+        {
+            std::cout << "buzz\n";
+        }
+        else
+        {
+            std::cout << i << std::endl;
         }
     }
     return 0;
