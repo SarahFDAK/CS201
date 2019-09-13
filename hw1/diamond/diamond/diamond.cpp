cb33c7430bb1b22e3afcdea8b9c26262ff5d63ce
Sarah Carter <sarah.c@fdak.net>
2019-09-12 20:39:05 -0800

    Commit diamond.cpp

---
+++ hw1/diamond/diamond/diamond.cpp
@@ -0,0 +1,19 @@
+//
+//  diamond.cpp
+//  Sarah Carter
+//  9/12/19
+//  This program will prompt the user to input an integer, then create a diamond pattern with a corresponding number of lines.
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    int user_num = 0; //Initialize and assign a variable to receive user input
+    int i = 0;   //Initialize and assign a counter variable
+    
+    std::cout << "Please enter an integer to build a diamond pattern: \n";
+    std::cin >> user_num;
+    
+    
+    return 0;
+}
