14d873152bac6fb98ea8a58123dd9ba25c718205
Sarah Carter <sarah.c@fdak.net>
2019-09-13 09:41:29 -0800

    Commit lab05

---
+++ Labs/lab05/lab05/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab05
+//
+//  Created by Sarah Carter on 9/13/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
