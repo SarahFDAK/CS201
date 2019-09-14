466a68a8730130ccbe83218c18b00bd0ab66bec0
Sarah Carter <sarah.c@fdak.net>
2019-09-13 21:30:11 -0800

    Added comments and error message to mileskm.cpp

--- hw1/mileskm/mileskm/mileskm.cpp
+++ hw1/mileskm/mileskm/mileskm.cpp
@@ -8,11 +8,17 @@
 
 int main(int argc, const char * argv[]) {
     
+    //I decided to store the miles and kilometers as doubles instead of integers so the calculation can be more accurate.
     double miles = 0;
     double km = 0;
     
     std::cout << "Enter a number of miles you want converted to kilometers: \n";
     std::cin >> miles;
+    if(miles <= 0)
+    {
+        std::cout << "That is not a realistic conversion.\n";
+        return 0;
+    }
     km = 1.609 * miles;
     std::cout << miles << " miles is equivalent to " << km << " kilometers." << std::endl;
     return 0;
