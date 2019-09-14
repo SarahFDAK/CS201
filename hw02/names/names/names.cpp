753097010290d0c1bfcc26f5c1e4ca82e871c0d2
Sarah Carter <sarah.c@fdak.net>
2019-09-13 21:52:07 -0800

    Commit names.cpp

---
+++ hw02/names/names/names.cpp
@@ -0,0 +1,13 @@
+//
+//  names.cpp
+//  Sarah Carter
+//  9/13/19
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
