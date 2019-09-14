fef448d2ec58e40981e4948f306772574018a951
Sarah Carter <sarah.c@fdak.net>
2019-09-13 21:18:35 -0800

    Added comments to greatest.cpp

--- hw1/greatest/greatest/greatest.cpp
+++ hw1/greatest/greatest/greatest.cpp
@@ -6,34 +6,38 @@
 
 #include <iostream>
 
+//define my function prototype
 int entrynum();
 
 int main(int argc, const char * argv[]) {
     int greater = 0;
     
-    if(entrynum() <= 0)
+    if(entrynum() <= 0) //Check if the number entered is greater than 0.
     {
+        //If not greater than 0, inform the user and exit the program.
         std::cout << "You didn't enter a positive number.\n";
     }
     else
     {
-        while(entrynum() > 0)
+        while(entrynum() > 0) //If user entry is greater than zero, enter the while loop
         {
-            int holder = entrynum();
-            if(holder > greater)
+            int holder = entrynum(); //set input number to a local variable "holder"
+            if(holder > greater) //Check if new number is greater than "greater" variable
             {
-                greater = holder;
+                greater = holder; //if so, replace "greater" with the new number. Otherwise, leave it as its previous value.
             }
         }
     }
+    //Once the user has entered 0 or a negative number, tell the largest number entered.
     std::cout << "The greatest number you entered was " << greater << "." << std::endl;
     return 0;
 }
 
+//Function that asks for number inputs and assigns them to a variable "entry" that is returned to main.
 int entrynum()
     {
         int entry = 0;
-        std::cout << "Enter a number: " <<std::endl;
+        std::cout << "Enter a positive number. Enter 0 or a negative number to exit the program." <<std::endl;
         std::cin >> entry;
         return entry;
     }
