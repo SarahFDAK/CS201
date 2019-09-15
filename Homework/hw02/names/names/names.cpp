2e4c0039acbd3eece082b02f89ccf21e825706a1
Sarah Carter <sarah.c@fdak.net>
2019-09-15 11:56:25 -0800

    Commit names.cpp

--- Homework/hw02/names/names/names.cpp
+++ Homework/hw02/names/names/names.cpp
@@ -5,9 +5,21 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<std::string> names;
+    for(int i = 0; i < 10; i++)
+    {
+        std::string name;
+        std::cout << "Please enter a name: ";
+        std::getline(std::cin, name);
+        names.push_back(name);
+    }
     return 0;
 }
+
+
