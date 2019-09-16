dd84251d5b94fe729881c9d825a68b152417b19d
Sarah Carter <sarah.c@fdak.net>
2019-09-16 09:47:32 -0800

    Commit lab06

---
+++ Labs/lab06/lab06/lab06.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  lab06
+//
+//  Created by Sarah Carter on 9/16/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+void stringInfo(string input);
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+void stringInfo(string input){
+    
+}
