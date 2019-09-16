8695a9dac41e55f80a471651e57fed36c98899e8
Sarah Carter <sarah.c@fdak.net>
2019-09-16 10:01:44 -0800

    Commit lab06 pt 2

--- Labs/lab06/lab06/lab06.cpp
+++ Labs/lab06/lab06/lab06.cpp
@@ -13,16 +13,33 @@ using std::cout;
 using std::endl;
 
 void stringInfo(string input);
+void stringHash(string input);
 
 int main(int argc, const char * argv[]) {
     string input;
     cout << "What word do you want printed?" << endl;
     std::getline(std::cin, input);
     stringInfo(input);
+    stringHash(input);
     return 0;
 }
 
 void stringInfo(string input){
     float length = input.size();
     cout << "Your string " << input << " is " << length << " characters long." << endl;
+}
+
+void stringHash(string input){
+    for(float i = 0; i < input.size(); i++){
+        cout << "#";
+    }
+}
+
+void stringReverse(string input){
+    float length = input.size();
+    for(int i = 0; i < input.size(); i++){
+        for(float j = input.size()-1; j >=0; j--){
+            
+        }
+    }
 }
