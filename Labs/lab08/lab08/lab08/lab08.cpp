c7adc937cafe3d538e6674243a9c56693d7f67ee
Sarah Carter <sarah.c@fdak.net>
2019-09-20 09:57:06 -0800

    Commit completed lab08

--- Labs/lab08/lab08/lab08/lab08.cpp
+++ Labs/lab08/lab08/lab08/lab08.cpp
@@ -15,5 +15,16 @@ int doInput() {
 }
 
 int compute(int n){
-    return n*n;
+    if(n<=0){
+        return 0;
+    }
+    else{
+        int count = 0; //declare counter variable
+        for(int i = n-1; i > 0; i--){
+            if(n%i ==0){
+                count++;
+            }
+        }
+        return count;
+    }
 }
