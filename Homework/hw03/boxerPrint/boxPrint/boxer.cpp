a1a8fc38adfd938bfc6e0f8e6e6d304be729b29c
Sarah Carter <sarah.c@fdak.net>
2019-09-23 21:34:41 -0800

    Added extra functions

--- Homework/hw03/boxerPrint/boxPrint/boxer.cpp
+++ Homework/hw03/boxerPrint/boxPrint/boxer.cpp
@@ -9,46 +9,42 @@
 #include "boxer.hpp"
 #include <iostream>
 
+void printMiddle(int userInt, int length);
+void printAllAsterisks(int userInt, int length);
+void printSideStars(int userInt, int length);
+
 void boxer(std::string userWord, int userInt){
     int length = userWord.size();
-    for (int i = 0; i < userInt; i++){
-        for (int j = 0; j < (2*userInt)+length +2; j++){
-            std::cout << "*";
-        }
-        std::cout << std::endl;
-    }
-    for (int k = 0; k < userInt; k++){
-        std::cout << "*";
-    }
-    for (int l = 0; l < (length + 2); l++){
-        std::cout << " ";
-    }
-    for (int k = 0; k < userInt; k++){
-        std::cout << "*";
-    }
-    std::cout << std::endl;
-    for (int k = 0; k < userInt; k++){
-        std::cout << "*";
-    }
+    printAllAsterisks(userInt, length);
+    printMiddle(userInt, length);
+    printSideStars(userInt, length);
     std::cout << " " << userWord << " ";
-    for (int k = 0; k < userInt; k++){
-        std::cout << "*";
-    }
+    printSideStars(userInt, length);
     std::cout << std::endl;
-    for (int k = 0; k < userInt; k++){
-        std::cout << "*";
-    }
+    printMiddle(userInt, length);
+    printAllAsterisks(userInt, length);
+}
+
+void printMiddle(int userInt, int length){
+    printSideStars(userInt, length);
     for (int l = 0; l < (length + 2); l++){
         std::cout << " ";
     }
-    for (int k = 0; k < userInt; k++){
-        std::cout << "*";
-    }
+    printSideStars(userInt, length);
     std::cout << std::endl;
+}
+
+void printAllAsterisks(int userInt, int length){
     for (int i = 0; i < userInt; i++){
         for (int j = 0; j < (2*userInt)+length +2; j++){
             std::cout << "*";
         }
         std::cout << std::endl;
+    }
+}
+
+void printSideStars(int userInt, int length){
+    for (int k = 0; k < userInt; k++){
+        std::cout << "*";
     }
 }
