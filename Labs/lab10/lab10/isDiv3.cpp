86937fe421c1e44ac1f98659ae7f66950a762075
Sarah Carter <sarah.c@fdak.net>
2019-09-25 10:00:22 -0800

    Created lab10

---
+++ Labs/lab10/lab10/isDiv3.cpp
@@ -0,0 +1,9 @@
+//
+//  isDiv3.cpp
+//  lab10
+//
+//  Created by Sarah Carter on 9/25/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include "isDiv3.hpp"
---
+++ Labs/lab10/lab10/isDiv3.hpp
@@ -0,0 +1,16 @@
+//
+//  isDiv3.hpp
+//  lab10
+//
+//  Created by Sarah Carter on 9/25/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#ifndef isDiv3_hpp
+#define isDiv3_hpp
+
+#include <stdio.h>
+
+bool isDiv3(int n);
+
+#endif /* isDiv3_hpp */
