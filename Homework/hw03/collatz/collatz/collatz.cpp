b6c7d9e5a017f5627f3707346092d174b2fa51fc
Sarah Carter <sarah.c@fdak.net>
2019-09-24 21:23:12 -0800

    Added comments

--- Homework/hw03/collatz/collatz/collatz.cpp
+++ Homework/hw03/collatz/collatz/collatz.cpp
@@ -9,16 +9,21 @@
 
 int main(int argc, const char * argv[]) {
     int c = 0;
+    //Request integer
     std::cout << "Please enter a postive integer (enter 0 to exit): \n";
     std::cin >> c;
+    //Start loop if user enters non-zero number
     while(c != 0){
         std::cout << c << " ";
+        //Check if number is positive. If not, request a new number.
         while(c < 0){
             std::cout << "Please enter a positive integer (enter 0 to exit): \n";
             std::cin.clear();
             std::cin >> c;
         }
+        //Print Collatz sequence, stopping when 1 is reached
         while(c > 1){
+            //Check if number is even or odd
             if(c%2 != 0){
                 c = 3*c +1;
             }
