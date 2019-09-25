465ca5e1f7213b66b1dbdd72f0df13d7c5eb511c
Sarah Carter <sarah.c@fdak.net>
2019-09-24 21:16:22 -0800

    Added program loop to collatz.cpp

--- Homework/hw03/collatz/collatz/collatz.cpp
+++ Homework/hw03/collatz/collatz/collatz.cpp
@@ -9,23 +9,30 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    int c = 0;
+    int a = 0;
     std::cout << "Please enter a postive integer: \n";
-    std::cin >> c;
-    
-    while(c < 0){
-        std::cout << "Please enter a positive integer: ";
+    std::cin >> a;
+    while(a != 0){
+        std::cout << a << " ";
+        int c = a;
+        while(c < 0){
+            std::cout << "Please enter a positive integer: ";
+            std::cin.clear();
+            std::cin >> c;
+        }
+        while(c > 1){
+            if(c%2 != 0){
+                c = 3*c +1;
+            }
+            else{
+                c = c/2;
+            }
+            std::cout << c << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "Please enter a positive integer: \n";
         std::cin.clear();
-        std::cin >> c;
-    }
-    while(c >= 1){
-        if(c%2 == 0){
-            c = 3*c +1;
-        }
-        else{
-            c = c/2;
-        }
-        std::cout << c << " ";
+        std::cin >> a;
     }
     return 0;
 }
