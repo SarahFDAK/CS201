394d64dafef66fe6aa8f76a40c4f6bb3e14b5784
Sarah Carter <sarah.c@fdak.net>
2019-09-24 21:20:59 -0800

    Removed superfluous variable

--- Homework/hw03/collatz/collatz/collatz.cpp
+++ Homework/hw03/collatz/collatz/collatz.cpp
@@ -1,22 +1,20 @@
 //
-//  main.cpp
-//  collatz
-//
-//  Created by Sarah Carter on 9/24/19.
-//  Copyright © 2019 Sarah Carter. All rights reserved.
+//  collatz.cpp
+//  Sarah Carter
+//  9/24/19
+//  This program asks the user for a positive integer, checks to make sure it is positive and non-zero, then prints the Collatz sequence that starts with that number. It exits when the user enters 0.
 //
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    int a = 0;
-    std::cout << "Please enter a postive integer: \n";
-    std::cin >> a;
-    while(a != 0){
-        std::cout << a << " ";
-        int c = a;
+    int c = 0;
+    std::cout << "Please enter a postive integer (enter 0 to exit): \n";
+    std::cin >> c;
+    while(c != 0){
+        std::cout << c << " ";
         while(c < 0){
-            std::cout << "Please enter a positive integer: ";
+            std::cout << "Please enter a positive integer (enter 0 to exit): \n";
             std::cin.clear();
             std::cin >> c;
         }
@@ -30,9 +28,9 @@ int main(int argc, const char * argv[]) {
             std::cout << c << " ";
         }
         std::cout << std::endl;
-        std::cout << "Please enter a positive integer: \n";
+        std::cout << "Please enter a positive integer (enter 0 to exit): \n";
         std::cin.clear();
-        std::cin >> a;
+        std::cin >> c;
     }
     return 0;
 }
