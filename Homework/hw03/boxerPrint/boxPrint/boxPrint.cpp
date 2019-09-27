b4c1c283e0ab8eee45169ff88466ca098162be52
Sarah Carter <sarah.c@fdak.net>
2019-09-27 12:04:38 -0800

    Updated boxerPrint.cpp

--- Homework/hw03/boxerPrint/boxPrint/boxPrint.cpp
+++ Homework/hw03/boxerPrint/boxPrint/boxPrint.cpp
@@ -11,21 +11,19 @@ int main(int argc, const char * argv[]) {
     //Get a word and an integer from the user
     std::string userWord;
     int userInt;
-    std::cout << "Please enter a word and a positive integer (enter a 0 to exit): " << std::endl;
-    std::cin >> userWord >> userInt;
-    //Check if they entered a positive number. If not, ask for one. Keep asking until they give a positive number or enter 0 to exit.
-    while(userInt < 0){
-        std::cout << "Please enter a positive integer, or 0 to exit: " << std::endl;
-        std::cin.clear();
-        std::cin >> userInt;
-    }
+    
     //If they enter a positive integer, print the box, then ask for new input.
-    while(userInt > 0){
-        boxer(userWord, userInt);
-        std::cout << "Please enter a word and a positive integer (enter a 0 to exit): " << std::endl;
-        std::cin.clear();
+    do {
+        std::cout << "Please enter a word and a positive integer (enter 'a 0' to exit): " << std::endl;
         std::cin >> userWord >> userInt;
-    }
+        //Check if they entered a positive number. If not, ask for one. Keep asking until they give a positive number or enter 0 to exit.
+        if(userInt < 0){
+            std::cin.clear();
+            continue;
+        }
+        boxer(userWord, userInt);
+        std::cin.clear();
+    } while(userInt > 0);
     //When they have entered 0, thank them and exit the program.
     std::cout << "Thank you for your participation!\n";
     return 0;
