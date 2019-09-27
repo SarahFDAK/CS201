1df7427cd5f87fb3c84cb819b0ad5e2657b9da8b
Sarah Carter <sarah.c@fdak.net>
2019-09-27 09:52:12 -0800

    Commit lab11

---
+++ Labs/lab11/lab11/lab11.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  lab11
+//
+//  Created by Sarah Carter on 9/27/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+int main(int argc, const char * argv[]) {
+    std::string word;
+    std::cout << "Enter a word: " << std::endl;
+    std::getline(std::cin, word);
+    for(int i = 0; i < word.size(); i++){
+        if(word[i] >= 'a' && word[i] <= 'z'){
+            std::cout << word[i] << std::endl;
+        }
+    }
+    return 0;
+}
