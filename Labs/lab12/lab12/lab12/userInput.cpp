e316de895408c25e6e597c4e2f531b233ff9511a
Sarah Carter <sarah.c@fdak.net>
2019-09-30 10:16:51 -0800

    Commit lab 12

---
+++ Labs/lab12/lab12/lab12/userInput.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  lab12
+//
+//  Created by Sarah Carter on 9/30/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+void getUserStrings(std::vector<std::string> & words) {
+    std::string input;
+    while(std::cin >>input){
+        if(input == "end"){
+            break;
+        }
+        else {
+            words.push_back(input);
+        }
+    }
+    for(int n = 0; n < words.size(); n++){
+        std::cout << words[n] << " ";
+    }
+}
