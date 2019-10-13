e70a276bd44a27ad595354b9e07862c2032277ae
Sarah Carter <sarah.c@fdak.net>
2019-10-13 15:32:15 -0800

    Create tokenizer.cpp and tokenizer.hpp

---
+++ Homework/hw04/hw04/tokenizer.cpp
@@ -0,0 +1,9 @@
+//
+//  tokenizer.cpp
+//  hw04
+//
+//  Created by Sarah Carter on 10/13/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include "tokenizer.hpp"
---
+++ Homework/hw04/hw04/tokenizer.hpp
@@ -0,0 +1,14 @@
+//
+//  tokenizer.hpp
+//  hw04
+//
+//  Created by Sarah Carter on 10/13/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#ifndef tokenizer_hpp
+#define tokenizer_hpp
+
+#include <stdio.h>
+
+#endif /* tokenizer_hpp */
