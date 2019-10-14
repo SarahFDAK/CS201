4242bb93557c12fc86f3f2acc4f8bd04e2c6a5a5
Sarah Carter <sarah.c@fdak.net>
2019-10-13 16:19:14 -0800

    Declare unsigned StringToTokensWS and void AnalyzeTokens in tokenizer.hpp

--- Homework/hw04/hw04/tokenizer.hpp
+++ Homework/hw04/hw04/tokenizer.hpp
@@ -13,5 +13,7 @@
 #include <iostream>
 
 bool ReadLine(std::string & str);
+unsigned StringToTokensWS(std::vector<std::string> & tokens);
+void AnalyzeTokens(const std::vector<std::string> & tokens);
 
 #endif /* tokenizer_hpp */
