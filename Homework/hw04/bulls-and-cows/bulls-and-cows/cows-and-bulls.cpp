43144495129d7dd4f5ec25ff34241e1e10a4c3d7
Sarah Carter <sarah.c@fdak.net>
2019-10-15 15:02:43 -0800

    Added comments

--- Homework/hw04/bulls-and-cows/bulls-and-cows/cows-and-bulls.cpp
+++ Homework/hw04/bulls-and-cows/bulls-and-cows/cows-and-bulls.cpp
@@ -1,34 +1,33 @@
 //
-//  main.cpp
-//  bulls-and-cows
-//
-//  Created by Sarah Carter on 10/15/19.
-//  Copyright © 2019 Sarah Carter. All rights reserved.
+//  bulls-and-cows.cpp
+//  Sarah Carter
+//  10/15/19
+//  This program requests four digits from the user and reports back (in the form of bulls and cows) how many correct digits the user's guess has and if they are in the correct positions.
 //
 
 #include <iostream>
 #include "guesscheck.hpp"
 
 int main(int argc, const char * argv[]) {
-    
-    std::string answer = "1234";
+    //Set answer and declare guess string
+    std::string answer = "5740";
     std::string guess;
-    
+    //Loop through guesses until there are 4 correct
     do {
         std::cout << "Guess what 4 digits are the answer. 4 bulls means you've guessed right, cows are the correct digit in the wrong place." << std::endl;
         
         std::cin >> guess;
-        
+        //Checks if guess is 4 characters long and if characters are integers
         if(!CheckGuessLength(guess) || !CheckGuessType(guess)){
             std::cin.clear();
             std::cout << "Please enter 4 integers." << std::endl;
             std::cin >> guess;
         }
-        
+        //Reports not totally correct answer status. CountCows function counts total number of correct numbers, regardless of placement, thus duplicating CountBulls; therefore the actual "cows" number is CountCows-CountBulls.
         std::cout << "Your guess has " << CountBulls(guess, answer) << " bulls and " << CountCows(guess, answer) - CountBulls(guess, answer) << " cows." << std::endl;
         
     } while(CountBulls(guess, answer) < answer.size());
-    
+    //Prints correct guess
     std::cout << guess << " is correct!" << std::endl;
     return 0;
 }
