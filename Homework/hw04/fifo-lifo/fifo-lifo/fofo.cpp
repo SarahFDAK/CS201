c359354d71d3a2bf87cd3c442817c6313a2735a4
Sarah Carter <sarah.c@fdak.net>
2019-10-15 15:05:48 -0800

    Added fofo.cpp and fofo.hpp

---
+++ Homework/hw04/fifo-lifo/fifo-lifo/fofo.cpp
@@ -0,0 +1,9 @@
+//
+//  fofo.cpp
+//  fifo-lifo
+//
+//  Created by Sarah Carter on 10/15/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include "fofo.hpp"
---
+++ Homework/hw04/fifo-lifo/fifo-lifo/fofo.hpp
@@ -0,0 +1,14 @@
+//
+//  fofo.hpp
+//  fifo-lifo
+//
+//  Created by Sarah Carter on 10/15/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#ifndef fofo_hpp
+#define fofo_hpp
+
+#include <stdio.h>
+
+#endif /* fofo_hpp */
