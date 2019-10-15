727710e3bc79dfe5e7b528226128d4a7b5743e11
Sarah Carter <sarah.c@fdak.net>
2019-10-15 14:24:59 -0800

    Added guesscheck.cpp and .hpp

---
+++ Homework/hw04/bulls-and-cows/bulls-and-cows/guesscheck.cpp
@@ -0,0 +1,9 @@
+//
+//  guesscheck.cpp
+//  bulls-and-cows
+//
+//  Created by Sarah Carter on 10/15/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include "guesscheck.hpp"
---
+++ Homework/hw04/bulls-and-cows/bulls-and-cows/guesscheck.hpp
@@ -0,0 +1,14 @@
+//
+//  guesscheck.hpp
+//  bulls-and-cows
+//
+//  Created by Sarah Carter on 10/15/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#ifndef guesscheck_hpp
+#define guesscheck_hpp
+
+#include <stdio.h>
+
+#endif /* guesscheck_hpp */
