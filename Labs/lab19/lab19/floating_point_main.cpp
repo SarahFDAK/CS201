9612504701293bf328b1b0bf3d23c76078d24abb
Sarah Carter <sarah.c@fdak.net>
2019-10-27 10:59:26 -0800

    Commit whatever in lab19 seems to be uncommitted

---
+++ Labs/lab19/lab19/floating_point_main.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file     L19_Floating_Point.cpp
+ * @author   Student Name
+ * @version  Jan 28, 2019
+ * John Quan 
+ *
+ * Useful trigonometric functions.
+ */
+
+#include <iostream>
+#include <vector>
+// using sin, cos
+#include <math.h>
+#include <cstdlib>
+
+#include "floating_point.hpp"
+
+int main() {
+	// TODO Turn on "error on warnings" so this doesn't*****************
+	// compile.  Make it compile by using std::size_t
+	// You can comment this for loop out after that.
+	std::vector<int> v { 1, 2, 3, 4, 5 };
+//	for (std::size_t i = 2; i < v.size(); ++i) {
+//		++v[i];
+//		std::cout << v[i] << std::endl;
+//	}
+	std::cout << std::endl << std::endl;
+    int degree = 45;
+
+	// *****************************************************************
+
+	// TODO Declare and initialize a vector of radians by calling*******
+	//        degreesToRadians(360).
+	//      Print degree,cos(radian),sin(radian) as a comma separated
+	//        values (CSV) list.
+//    for(int degree = 0; degree <= 360; degree++){
+//        std::cout << degree << "," << degreesToRadians(degree) <<
+////        "," << cos(degreesToRadians(degree)) << "," << sin(degreesToRadians(degree)) <<
+//        std::endl;
+//    }
+        
+	// *****************************************************************
+
+	return 0;
+}
