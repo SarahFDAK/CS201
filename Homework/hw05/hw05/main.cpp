f10b4c108015823ea592e4783725539eb44aad0b
Sarah Carter <sarah.c@fdak.net>
2019-10-27 18:43:50 -0800

    Created a text box and a button... don't know how to really make them do anything yet.

--- Homework/hw05/hw05/main.cpp
+++ Homework/hw05/hw05/main.cpp
@@ -1,15 +1,20 @@
-//
-//  main.cpp
-//  hw05
-//
-//  Created by Sarah Carter on 10/27/19.
-//  Copyright © 2019 Sarah Carter. All rights reserved.
-//
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Button.H>
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(int argc, char **argv) {
+    Fl_Window *window = new Fl_Window(340,180);
+//    Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
+//    box->box(FL_UP_BOX);
+//    box->labelfont(FL_BOLD+FL_ITALIC);
+//    box->labelsize(36);
+//    box->labeltype(FL_SHADOW_LABEL);
+    Fl_Input *entry = new Fl_Input(100, 10, 200, 50, "User Input");
+    entry->value();
+    Fl_Button *quit = new Fl_Button(100,100,75,40, "Quit");
+    window->end();
+    window->show(argc, argv);
+    return Fl::run();
 }
