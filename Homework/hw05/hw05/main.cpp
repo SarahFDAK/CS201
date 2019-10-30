e9ccee63f0fefbdab864b65be5775bf2cc34af96
Sarah Carter <sarah.c@fdak.net>
2019-10-30 10:48:31 -0800

    Commit help from Dr. Metzgar

--- Homework/hw05/hw05/main.cpp
+++ Homework/hw05/hw05/main.cpp
@@ -6,36 +6,52 @@
 #include <FL/Fl_Output.H>
 #include <FL/Fl_Button.H>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 #include "truncstruct.hpp"
+
+StringInfo userStuff { "", 0 };
+
+Fl_Input *sizeInput = nullptr;
+Fl_Output *truncOutput = nullptr;
 
 void quit_cb(Fl_Widget* obj, void*){
     exit(0);
 }
 
-void truncBut_cb(Fl_Widget* trunc, StringInfo userInfo){
+void truncBut_cb(Fl_Widget* trunc, void* data){
+    
+}
 
+void sizeInputChanged(Fl_Widget *w, void *data)
+{
+    Fl_Input *input = (Fl_Input*)w;
+    std::string str = input->value();
+    std::istringstream istr(str);
+    istr >> userStuff.len;
+    std::cout << userStuff.len;
+    truncOutput->value(str.c_str());
 }
 
 int main(int argc, char **argv) {
     Fl_Window *window = new Fl_Window(340,180);
 
-    std::string input;
-    size_t len;
-    std::string output;
+//    std::string input;
+//    size_t len;
+//    std::string output;
     window->begin();
         Fl_Input *entry = new Fl_Input(100, 10, 200, 30, "User Input");
-        input = entry->value();
-        Fl_Input *size = new Fl_Input(100, 50, 100, 30, "# of chars" );
-        len = size_t(size->value());
-        Fl_Output *output = new Fl_Output(100, 90, 200, 30, "Output");
+        //input = entry->value();
+        sizeInput = new Fl_Input(100, 50, 100, 30, "# of chars" );
+    sizeInput->callback(sizeInputChanged, nullptr);
+        //len = size_t(size->value());
+        truncOutput = new Fl_Output(100, 90, 200, 30, "Output");
         Fl_Button *truncate = new Fl_Button(85, 130, 75, 40, "Truncate");
         Fl_Button *quit = new Fl_Button(185,130,75,40, "Quit");
     window->end();
-    StringInfo userStuff {input, len };
     
-    truncate->callback( (Fl_Callback*) truncBut_cb(truncate, userStuff);
+    //truncate->callback( truncBut_cb(truncate, userStuff)) ;
     quit->callback( (Fl_Callback*) quit_cb );
 
     window->show(argc, argv);
