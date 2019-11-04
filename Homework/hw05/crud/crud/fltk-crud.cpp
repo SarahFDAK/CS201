16c4422e434ab36e17266118fc0c6a351cc3a7eb
Sarah Carter <sarah.c@fdak.net>
2019-11-03 17:11:03 -0900

    Added structs,  window, and menu bar

--- Homework/hw05/crud/crud/fltk-crud.cpp
+++ Homework/hw05/crud/crud/fltk-crud.cpp
@@ -13,11 +13,27 @@
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Output.H>
 #include <FL/Fl_Button.H>
-#include <FL/Fl_Menu_H>
+#include <FL/Fl_Menu_Bar.H>
+#include <string>
+#include <map>
 
+struct Model{
+    std::string userInput;
+};
 
+struct View{
+    Fl_Input* input = nullptr;
+    Fl_Output* output = nullptr;
+};
+
+Fl_Window* WindowCreate(){
+    Fl_Window* win = new Fl_Window(600, 400);
+    Fl_Menu_Bar* menu = new Fl_Menu_Bar(10, 20, 560, 30);
+    return win;
+}
 
 int main(int argc, const char * argv[]) {
-
-    return 0;
+    Fl_Window* window = WindowCreate();
+    window->show();
+    return Fl::run();
 }
