85f090414730f379076d427f151eae441882338e
Sarah Carter <sarah.c@fdak.net>
2019-11-03 16:19:33 -0900

    Added FLTK headers to fltk-crud.cpp; deleted crud.cpp and crud.hpp

--- Homework/hw05/crud/crud/crud.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  crud.cpp
-//  crud
-//
-//  Created by Sarah Carter on 10/31/19.
-//  Copyright © 2019 Sarah Carter. All rights reserved.
-//
-
-#include "crud.hpp"
--- Homework/hw05/crud/crud/fltk-crud.cpp
+++ Homework/hw05/crud/crud/fltk-crud.cpp
@@ -7,6 +7,15 @@
 //
 
 #include <iostream>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Menu_H>
+
+
 
 int main(int argc, const char * argv[]) {
 
