8c82fcfcf25064d23c284f90dfacf852f7a4358c
Sarah Carter <sarah.c@fdak.net>
2019-11-09 22:17:36 -0900

    Created maps for each distribution and called print function for each

--- Homework/hw06/hw06/main.cpp
+++ Homework/hw06/hw06/main.cpp
@@ -43,36 +43,43 @@ int RandomBetween(int first, int last){
 }
 
 void PrintDistribution(const std::map<int,int> &numbers){
-    std::cout << "Normal distribution around " << mean << ":\n";
-    for (auto p : hist) {
+    for (auto p : numbers) {
         std::cout << std::fixed << std::setprecision(1) << std::setw(2)
                   << p.first << ' ' << std::string(p.second/200, '*') << '\n';
     }
+    std::cout << std::endl;
 }
 
 int main()
 {
-    // Seed with a real random value, if available
-    std::random_device r;
- 
-    // Choose a random mean between 1 and 6
-    std::default_random_engine e1(r());
-    std::uniform_int_distribution<int> uniform_dist(1, 6);
-    int mean = uniform_dist(e1);
-    std::cout << "Randomly-chosen mean: " << mean << '\n';
- 
-    // Generate a normal distribution around that mean
-    std::seed_seq seed2{r(), r(), r(), r(), r(), r(), r(), r()};
-    std::mt19937 e2(seed2);
-    std::normal_distribution<> normal_dist(mean, 2);
- 
+//    // Seed with a real random value, if available
+//    std::random_device r;
+//
+//    // Choose a random mean between 1 and 6
+//    std::default_random_engine e1(r());
+//    std::uniform_int_distribution<int> uniform_dist(1, 6);
+//    int mean = uniform_dist(e1);
+//    std::cout << "Randomly-chosen mean: " << mean << '\n';
+//
+//    // Generate a normal distribution around that mean
+//    std::seed_seq seed2{r(), r(), r(), r(), r(), r(), r(), r()};
+//    std::mt19937 e2(seed2);
+//    std::normal_distribution<> normal_dist(mean, 2);
+//
     std::map<int, int> hist;
+    std::map<int, int> histu;
+    std::map<int, int> histn;
     for (int n = 0; n < 10000; ++n) {
-        ++hist[std::round(RandomBetweenN(1, 6))];
+        ++histn[std::round(RandomBetweenN(1, 6))];
+        ++histu[std::round(RandomBetweenU(1, 6))];
+        ++hist[std::round(RandomBetween(1, 6))];
     }
-    std::cout << "Normal distribution around " << mean << ":\n";
-    for (auto p : hist) {
-        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
-                  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
-    }
+    
+    std::cout << "Uniform Distribution" << ":\n";
+    PrintDistribution(histu);
+    std::cout << "Normal Distribution" << ":\n";
+    PrintDistribution(histn);
+    std::cout << "Rand()" << ":\n";
+    PrintDistribution(hist);
+
 }
