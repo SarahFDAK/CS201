c6b08dee8e2d56a122c2f3d295dfa53e0b85b646
Sarah Carter <sarah.c@fdak.net>
2019-12-09 22:34:02 -0900

    tf.cpp reads the document, does not yet ignore characters.

--- Homework/hw08/tf/tf/tf.cpp
+++ Homework/hw08/tf/tf/tf.cpp
@@ -11,6 +11,7 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 using std::vector;
 using std::ofstream;
@@ -21,8 +22,10 @@ using std::string;
 using std::istringstream;
 
 const vector<string> stopWords {"the", "but", "and", "or", "for"};
+const vector<char> stopChars {'.', ',', '\"'};
 
 int main(int argc, const char * argv[]) {
+    std::map<string, int> words;
     ifstream fin("1661-0.txt");
     if(!fin){
         cout << "Error opening file." << endl;
@@ -34,14 +37,39 @@ int main(int argc, const char * argv[]) {
         if(!fin){
             if(fin.eof()){
                 cout << "Reached end of file." << endl;
-                return 0;
+                break;
             }
             else{
                 cout << "Error reading file" << endl;
                 break;
             }
         }
-        
+        istringstream read(line);
+        while(read){
+            string word;
+            read >> word;
+            std::transform(word.begin(), word.end(), word.begin(),
+                           [](char c){return std::tolower(c);});
+            for(char c:stopChars){
+                while(word[0] == c)
+                    word.erase(0,1);
+                while(word[word.size()-1] == c)
+                    word.pop_back();
+                cout << word << endl;
+            }
+            for(string n:stopWords){
+                if (n == word)
+                    continue;
+            }
+            if(words.count(word) == 0)
+                words[word] = 1;
+            else
+                words[word] += 1;
+        }
+    }
+    std::pair<int, string> flip;
+    for(auto i:words){
+        cout << i.first << " " << i.second << endl;
     }
     return 0;
 }
