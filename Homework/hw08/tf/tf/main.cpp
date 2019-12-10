801562d3aa314c3522451da34f08125a3e50dc8b
Sarah Carter <sarah.c@fdak.net>
2019-12-09 20:44:52 -0900

    Commit tf.cpp

---
+++ Homework/hw08/tf/tf/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  tf
+//
+//  Created by Sarah Carter on 12/9/19.
+//  Copyright © 2019 Sarah Carter. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
