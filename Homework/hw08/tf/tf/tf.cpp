0833418122a25fe235e6e67c686c8f36b1929a7a
Sarah Carter <sarah.c@fdak.net>
2019-12-11 19:30:23 -0900

    Possibly defeated stupid unicode characters.

--- Homework/hw08/tf/tf/tf.cpp
+++ Homework/hw08/tf/tf/tf.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <istream>
 
 using std::vector;
 using std::ofstream;
@@ -22,23 +23,37 @@ using std::string;
 using std::istringstream;
 
 const vector<string> stopWords {"the", "but", "and", "or", "for"};
-const vector<char> stopChars ('"', '\'', ',', '.', '!', '?');
+vector<char> stopChars {};
 
-//void fillStopChars(){
-//    for(char c0 = 0; c0 < 'A'; c0++)
-//        stopChars.push_back(c0);
-//
-//    for(char c = 91; c < 'a'; c++)
-//        stopChars.push_back(c);
-//
-//    for(char c1 = 123; c1 < 126; c1++){
-//        stopChars.push_back(c1);
-//    }
-//}
+void fillStopChars(){
+    for(char c0 = 33; c0 < 'A'; c0++)
+        stopChars.push_back(c0);
+
+    for(char c = 91; c < 'a'; c++)
+        stopChars.push_back(c);
+
+    for(char c1 = 123; c1 < 126; c1++){
+        stopChars.push_back(c1);
+    }
+}
+
+string fixString(string &word, const vector<char> &letters){
+    cout << word << endl;
+    string wordSub;
+    for(char c:word){
+        auto itv = find(letters.begin(), letters.end(), c);
+        if(itv != letters.end())
+            wordSub.push_back(c);
+    }
+    word = wordSub;
+    return word;
+}
 
 int main(int argc, const char * argv[]) {
-//    fillStopChars();
-    
+    fillStopChars();
+    vector<char> letters;
+    for(char l = 'a'; l <= 'z'; l++)
+        letters.push_back(l);
     std::map<string, int> words;
     ifstream fin("1661-0.txt");
     if(!fin){
@@ -64,17 +79,14 @@ int main(int argc, const char * argv[]) {
             read >> word;
             std::transform(word.begin(), word.end(), word.begin(),
                            [](char c){return std::tolower(c);});
-            for(char c:stopChars){
-                for(size_t i = 0; i < 3; i++){
-                    if(word[0] == c)
-                        word.erase(0,1);
-                }
-                for(size_t e = word.size() - 1; e > word.size()-2; e--){
-                    if(word[word.size()-1] == c)
-                        word.pop_back();
-                }
-                cout << word << endl;
-            }
+            fixString(word, letters);
+//            for(char c:stopChars){
+//                auto it = find(word.begin(), word.end(), c);
+//                if(*it == c){
+//                    word.erase(
+//                           remove(word.begin(), word.end(), c));
+//                    }
+//                }
             for(string n:stopWords){
                 if (n == word)
                     continue;
